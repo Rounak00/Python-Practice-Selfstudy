c7965ece0eecc4e378e8bb1e7f685cd2ca5a96f8
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-12-06 02:52:48 +0530

    Create 2Variables.py

---
+++ 2Variables.py
@@ -0,0 +1,20 @@
+var = "Hello"
+print(var)#so in python we dont need any datatype intialization
+
+
+print(type(var))#print the datatype
+
+#typecasting:
+var2="32"
+print(int(var2))
+
+
+print(10*"Hello")#print ten times, only valid by string
+
+
+
+#take in put
+ var name= input("Enter your num") # in this way input taken as string
+ varname= int(input("Enter your number"))#in this way we can take number input and for other datatypes we need to specify datatype here
+
+
