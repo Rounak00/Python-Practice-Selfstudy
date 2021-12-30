b142ae7ed9370c9c7b4fc6f648aa3a681ce6918f
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-12-31 05:15:14 +0530

    Create 7ConditionalStatements.py

---
+++ 7ConditionalStatements.py
@@ -0,0 +1,21 @@
+var= int(input())
+
+if var%2==0:
+     print("even")
+else:
+  print("odd")
+# we use else if as elif
+
+li=[1,2,3,4,5]
+
+if 5 in li:     
+  print("found")
+ #we have both 'not in' and 'in' keywords and its returns boolean values  
+
+
+
+#Switch case
+ switcher = {
+        0: "zero",
+        1: "one",
+        2: "two",}
