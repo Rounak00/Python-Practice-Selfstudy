7a97ddfd8df2d30172371f1b4937cbc42e23f87a
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-12-31 05:25:57 +0530

    Update 8Loops.py

--- 8Loops.py
+++ 8Loops.py
@@ -6,3 +6,31 @@ li=[1,2,3,4,5]
  li2=[["odd",1],["even",2],["zero",0]]
 for item, num in li2:
   print(item,num)
+  
+  
+"""while loop"""
+i=1
+while(i<10):
+ print(i)
+ i++
+
+ 
+ 
+ """Break and Continue"""
+ 
+ i=1
+ while(true):
+  print(i)
+  i++
+  if i>10:
+   break()#terminate the loop
+   
+   
+   
+ i=1
+ while(true)
+ i++
+ if i%5==0:
+  continue()#skip for those numbers that are divisable by 5
+  
+print(i)  
