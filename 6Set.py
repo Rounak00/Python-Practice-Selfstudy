2f1fcf04e6274ab463bd23712c8caf4541cec860
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-12-31 05:07:31 +0530

    Update and rename 6Sets.py to 6Set.py

---
+++ 6Set.py
@@ -0,0 +1,12 @@
+
+
+"""intialize-> st=set([]) or l=[], st=set(l);""" 
+st=set([1,2,3,4]);
+
+st.add(7)
+st.add(7)
+st.add(7)
+#but only take one 7
+
+"""we can perform union and intersection and disjoint by union() and intersextion() and isdisjoint() functions"""
+
