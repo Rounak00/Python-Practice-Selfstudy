bb996c7e652b305ab8d85ebad1521128872c7c44
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-12-31 02:53:17 +0530

    Update 2Variables.py

--- 2Variables.py
+++ 2Variables.py
@@ -18,3 +18,4 @@ print(10*"Hello")#print ten times, only valid by string
  varname= int(input("Enter your number"))#in this way we can take number input and for other datatypes we need to specify datatype here
 
 
+#and we have normal arithmatic sign for calculations like +,-,*,/,%, ** (Exponent/use as power), // (Floor Division/ 9//2 = 4 and 9.0//2.0 = 4.0, -11//3 = -4, -11.0//3 = -4.0)
