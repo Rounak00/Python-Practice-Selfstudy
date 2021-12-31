1a936df6ddf3599726b048f40e089154682849eb
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-12-31 14:39:47 +0530

    Create 13Recursion.py

---
+++ 13Recursion.py
@@ -0,0 +1,24 @@
+
+def factorial_recursive(n):
+    """
+        :param n: Integer
+        :return: n * n-1 * n-2 * n-3.......1
+    """
+    if n ==1:
+        return 1
+    else:
+        return n * factorial_recursive(n-1)
+    # 5 * factorial_recursive(4)
+    # 5 * 4 * factorial_recursive(3)
+    # 5 * 4 * 3 * factorial_recursive(2)
+    # 5 * 4 * 3 * 2 * factorial_recursive(1)
+    # 5 * 4 * 3 * 2 * 1 = 120
+
+# 0 1 1 2 3 5 8 13
+def fibonacci(n):
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibonacci(n-1)+ fibonacci(n-2)
