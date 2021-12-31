c23390a0142f2209bab0925fe831daa297c016b1
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-12-31 17:04:28 +0530

    Create 15Modules.py

---
+++ 15Modules.py
@@ -0,0 +1,8 @@
+import module_name #add modules like this in program
+
+"""Example"""
+import random
+random_number=random.randint(0,5)#random number from 0 to 5
+
+
+rand=random.random()#o-1 any number generate
