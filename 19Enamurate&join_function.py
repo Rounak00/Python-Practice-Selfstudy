829fd863f20ad2f89b2dde0fd85a076122672d8b
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-01-10 04:18:23 +0000

    Update and rename 19Enamurate_function.py to 19Enamurate&join_function.py

--- 19Enamurate&join_function.py
+++ 19Enamurate&join_function.py
@@ -1,3 +1,4 @@
+#Enamurate
 list_2 = ["Python", "Programming", "Is", "Fun"]
 #Counter value starts from 5
 result = enumerate(list_2, 5)
@@ -6,3 +7,14 @@ print(list(result))
 #We will get the following output:
 
 [(5, 'Python'), (6, 'Programming'), (7, 'Is'), (8, 'Fun')]
+
+
+
+
+#join
+#join() with lists
+numList = ['1', '2', '3', '4']
+separator = ', '
+print(separator.join(numList))
+Output:
+1, 2, 3, 4
