440e77a3aef3d5b3bcb9ce141ca94797008a62c5
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2026-01-30 15:32:19 +0530

    add some data in vector store notes

--- 4.0Langchain/6.0VectorStore.py
+++ 4.0Langchain/6.0VectorStore.py
@@ -3,3 +3,5 @@
 # Store is  a system with retirval and if we have DB features like backup and restore, ACID, Concurrency those are vector DB
 
 # nothing here mostly there some functions same for all vector store or DB in langchain 
+
+# we use different embedding models here to create embeddings
