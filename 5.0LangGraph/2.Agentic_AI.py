5249a21d3091ebe5f4a77664a7a0d5cc7d534efd
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2026-02-24 18:56:04 +0530

    Document Agentic AI concepts and components
    Added a detailed explanation of Agentic AI, including its characteristics and components.

---
+++ 5.0LangGraph/2.Agentic_AI.py
@@ -0,0 +1,33 @@
+What is Agentic AI ? 
+>  a AI that take task or goal from human and work towards completing it on its own with minimal human guidance.
+It take actions, adapt to change and seek helps only when necessery.
+
+Key characteristics -> autonomous, goal oriented, planning, Reasoning, Adaptibility, Context Awarness
+
+1. Automnomy -> Things doing automatically.
+               Way of control autonnomy -> Permission scope, HITL, Override Control, Guardrails/policies
+
+2. Goal oriented -> always keep persistent objective goal in mind, goals can be set with constraints and can altered. Goals store in Core Memory.
+
+3. Planning -> a way to break a high level goal in small goals, first plannign and then execution in a iterative way. 
+              Create multiple plans that is candidate plans, evaluations of plans, select best plan with the help of HITL & A peer programmed Policy.
+
+4.Reasoning -> Agentic ai system interpretes information, draws conclusion and make des=cisions - both while plannig and executing.
+
+           Reasoning in planning -> Goal Decomposition, Tool Selection, Resource Estimation
+           Reasoning in Execution -> Decision making, HITL Handling, Error handling
+
+5. Adaptibility -> External Feedback, Changing Goals, Failures
+
+6. Context Awareness -> 
+      Type of context -> Original goal, pogress till now + Interaction history, Tool Response, User Specific preference, Policy and Guardrail
+      Made by memory -> Short term and long term memory.
+
+
+Components of Agentic    AI 
+> Brain, orchestrator, tool, memory, supervisor
+> Brain is generally LLM
+> orchestrator execution of plans (Task sequence, retry logic, routing, deligation), manager of ai app.
+> Tools (hands and legs) knowlede base
+> Memory -> Long and Short memory ans state trackinhg
+> supervisor -> Approve request in HITL
