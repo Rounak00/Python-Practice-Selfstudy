c264cf4f1036cc07775d074f86c574ef50568936
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2026-02-24 17:07:34 +0530

    Introduce concepts of Generative and Agentic AI
    Added an overview of Generative AI and Agentic AI, including applications, problems, and solutions related to hiring processes.

---
+++ 5.0LangGraph/1.Generative_Vs_Agentic_AI.py
@@ -0,0 +1,33 @@
+Generative AI -> Create / generate new data like text, image, video. (chatgpt,gemini, claude,grok, dalE, codeLLama,sora, Evenlabs)
+Traditional AI -> Based on I/P and O/P find pattern and relationships and then give result (based on past weathers assume todays weather)
+>But GenAI not like tradition AI. Its learning distribution of data.
+
+Application areas of generative AI -> Bussiness writing, online education, software development, chatbot
+
+
+Problem -> "Hire a Backend engineer "
+1. create a JD
+2. post it in job platform
+3. shortlist applications
+4. Rolling an offer letter
+
+> Now doing whole task using genAI
+LLM Based chatbot we have  -> 
+  1. create a JD based on details
+  2. Chatbot can only say where the platforms i can post my job
+  3. based on jd Chatbot only can give guidence for shortlist
+  4. for schedule interview can only give emails
+
+Problems  -> Reactive (user tell each time what to do, chatbot dont have flow), chatbot dont have memory, Advice that chatbot give is very generic (It doesnot have company specific data), cant do work like post job automatically in platform
+Solution -> give a lot of past JDs, connect to knolwdge based, hiring startegy all can feed to the chatbot
+> Now this is a RAG based Chatbot (only solve geenric answer)
+
+
+Tool augmanted chatbot ->  using tools (Linkedin api, mail api, pdf parser, calender ) for perform tasks.
+> now perfomr actions but still problems are no memory and reactive. cant adapt new problem untill we told.
+
+Agentice AI (AI Agents) -> all will do automatically only ask for permissions.
+
+
+Generative AI is a capability and Agentic AI is a behaviour.
+
